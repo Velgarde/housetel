55ed3d0307183dd4a4cfc292ca315132f46a7f03
Akshat Dighade <akshat.dighade@gmail.com>
2024-04-06 18:45:54 +0530

    Add Rocket backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(non_snake_case)]
+#[macro_use] extern crate rocket;
+use rocket::fs::{FileServer, NamedFile};
+
+#[get("/")]
+async fn HomePage() -> Option<NamedFile> {
+    NamedFile::open("../dist/index.html").await.ok()
+}
+
+async fn DefaultPage() -> Option<NamedFile> {
+    NamedFile::open("../dist/default.html").await.ok()
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    //Assuming the build of the react file is in ../dist
+    rocket::build()
+    .mount("/", routes![HomePage])
+    .mount("/", FileServer::from("../dist"))
+    //.mount("/src", FileServer::from(""))
 }
